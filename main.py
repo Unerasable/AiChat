34df3d677a6a64d2b4107811d385b746aefba20d
Unerasable <shayanariz99@gmail.com>
2021-12-21 11:30:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ os.system('pip install python-dotenv')
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
-with randomstuff.Client(api_key=API_KEY) as client:
-  response = client.get_ai_response(input())
-  print(response.message)
+while True:
+  with randomstuff.Client(api_key=API_KEY) as client:
+    response = client.get_ai_response(input())
+    print(response.message)
